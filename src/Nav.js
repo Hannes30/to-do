7687c0b9abbf9daeca0a14e8da757d227d75fbd3
Hannes <hannes-scheibelauer@sz-ybbs.ac.at>
2022-07-07 21:12:38 +0200

    Nav

---
+++ src/Nav.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+class Nav extends React.Component {
+  render() {
+    return (
+      <div id="nav">
+        <img src="/Images/logo.png" alt="DO-DO logo" />
+        <h1>DO-DO</h1>
+        <p id="strich"></p>
+      </div>
+    );
+  }
+}
+
+export default Nav;
