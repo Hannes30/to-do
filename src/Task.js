31883e61338844006826217a923d404ceb8df4b9
Hannes <hannes-scheibelauer@sz-ybbs.ac.at>
2022-07-31 22:38:06 +0200

    v0.0

--- src/Task.js
+++ src/Task.js
@@ -16,6 +16,12 @@ function Task(props) {
   };
   return (
     <div className="Task">
+      <svg xmlns="http://www.w3.org/2000/svg" className="check_circel">
+        <g fill="transparent">
+          <circle stroke="black" strokeWidth="2" cx="20" cy="20" r="20" />
+          <path d="M520.5 78.1z" />
+        </g>
+      </svg>
       <h2 className="Task-Name">
         <span>{props.Title}</span>
         <p className="Task-Time">{TimteToString()}</p>
