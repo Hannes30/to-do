ae678d539872122b2c458825b6da42f312b18d96
Hannes <hannes-scheibelauer@sz-ybbs.ac.at>
2022-08-21 14:48:52 +0200

    yeah

--- src/Login.js
+++ src/Login.js
@@ -4,7 +4,7 @@ import { useEffect } from "react";
 var errormsg;
 const url = "https://hannesscheibelauer-dodo-backen.herokuapp.com";
 const fot = {
-  mode: "no-cors",
+  mode: "no-cors"
 };
 
 function Login(props) {
