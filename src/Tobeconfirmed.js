83b44e757bdd5772e1d57a88b4c3d68f62eaa72b
Hannes30 <hannes.scheibelauer@sz-ybbs.ac.at>
2022-09-19 09:59:35 +0200

    to be confirmed und shit

--- src/Tobeconfirmed.js
+++ src/Tobeconfirmed.js
@@ -3,7 +3,19 @@ import React from "react";
 function Tobeconfirmed(props) {
   return (
     <div id="tobeconfirmed">
-      <h2>Email zu account aktivierung wurde an {props.email} gesendet</h2>
+      <h2>Email zu account aktivierung wurde an {props.email} gesendet.</h2>
+      <br />
+      <p>falsche email?</p>
+      <button
+        id="register-new"
+        onClick={() => {
+          window.localStorage.clear();
+          window.location.reload();
+        }}
+      >
+        {" "}
+        Neu Regestrieren
+      </button>
     </div>
   );
 }
